2b7ecf828dbb659ab9a8db687ed3f09138ef7d00
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-14 17:10:47 +0600

    complite the text editor site

--- challange-3.js
+++ challange-3.js
@@ -1,30 +1,61 @@
 const textElement=document.getElementById('text-filed');
 
 
-function change(commandBtn) {
-    const getClassName =textElement.classList.add(commandBtn);
+function change(commandBtn, btnId) {
+    
+    const getClassName =textElement.classList;
+    const getBtnClassName =document.getElementById(btnId).classList;
+    // const getClassName =textElement.classList.add(commandBtn);
+    const getClassNameArray = Object.values(getClassName);
+
+    if (getClassNameArray.includes(commandBtn)) {
+        getClassName.remove(commandBtn);
+        getBtnClassName.remove('bg-sky-400','text-white')
+    }else{
+        getClassName.add(commandBtn);
+        getBtnClassName.add('bg-sky-400','text-white')
+    }   
 }
+
 document.getElementById('bold-btn').addEventListener('click', function () {
-    change('font-bold')
+    change('font-bold','bold-btn')
 })
 document.getElementById('italic-btn').addEventListener('click', function () {
-    change('italic')
+    change('italic','italic-btn')
 })
 document.getElementById('underline-btn').addEventListener('click', function () {
-    change('underline')
+    change('underline','underline-btn')
 })
 document.getElementById('align-start-btn').addEventListener('click', function () {
-    change('align-start')
+    change('align-start','align-start-btn')
 })
 document.getElementById('align-center-btn').addEventListener('click', function () {
-    change('text-center')
+    change('text-center','align-center-btn')
 })
 document.getElementById('align-left-btn').addEventListener('click', function () {
-    change('text-end')
+    change('text-end','align-left-btn')
 })
 document.getElementById('align-justify-btn').addEventListener('click', function () {
-    change('text-justify')
+    change('text-justify','align-justify-btn')
 })
+
+
+document.getElementById('font-size-btn').addEventListener('keyup',function () {
+    document.getElementById('font-size-btn')
+})
+function changeFontSize() {
+    const fontNumber = document.getElementById("font-size-btn");
+    const value = fontNumber.value;
+    textElement.style.fontSize = (value+1) + "px";
+    }
+
+
 document.getElementById('case-btn').addEventListener('click', function () {
-    change('uppercase')
+    change('uppercase','case-btn')
+})
+document.getElementById('text-color-btn').addEventListener('blur',function () {
+    const colorValue = document.getElementById('text-color-btn').value;
+    const creatClassName ='text-['+colorValue+']';
+    change(creatClassName, 'text-color-btn')
+    
 })
