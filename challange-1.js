0e471aa54529995129ed641d37296ad4a3c0e00a
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-15 20:01:53 +0600

    complete the withdraw and deposit site

--- challange-1.js
+++ challange-1.js
@@ -1,21 +1,49 @@
 
 
-function getAndSet(inputId, setId) {
+function getAndSet(inputId, setId,isDeposit) {
     // get value
-    const InputFiled=document.getElementById(inputId);
-    const inputValueString=InputFiled.value;
+    const inputFiled=document.getElementById(inputId);
+    if (isNaN(inputFiled.value)) {
+        inputFiled.value='';
+        alert('input a number')
+        return;
+    }
+    const inputValueString=inputFiled.value;
+    inputFiled.value='';
+    
     const inputValue=parseFloat(inputValueString);
     // get deposit set value and location
     const getSetLocation=document.getElementById(setId);
     const setValueString=getSetLocation.innerText;
     const setValue=parseFloat(setValueString);
-    //get balance set value and location
 
     // set 
-    getSetLocation.innerText=inputValue+setValue;
-
-}
-document.getElementById('deposit-btn').addEventListener('click',function () {
-    getAndSet('deposit-filed', 'deposit-total')
+    if (setValue <= 0) {
+        return;
+    }else{
+        getSetLocation.innerText=inputValue + setValue;
+    }
+    //get balance set value and location
+    const getBalanceLocation=document.getElementById('balance-total');
     
+    const balanceTotalString=getBalanceLocation.innerText;
+    const balanceTotal=parseFloat(balanceTotalString);
+
+    if (isDeposit===true) {
+        
+        getBalanceLocation.innerText=balanceTotal + inputValue;
+    } else if(isDeposit===false){
+        
+        getBalanceLocation.innerText=balanceTotal - inputValue;
+    }
+    
+}
+
+document.getElementById('deposit-btn').addEventListener('click',function () {
+    getAndSet('deposit-filed', 'deposit-total',true)
+    
+})
+
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    getAndSet('withdraw-filed', 'withdraw-total',false)
 })
